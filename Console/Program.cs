cd678e7a4d4e60a03787805544ee8e09b34e652e
ngoan18 <Buikimngoan240@gmail.com>
2023-08-22 09:41:22 +0700

    BTHcau lenh SWITCH-CASE

--- Console/Program.cs
+++ Console/Program.cs
@@ -91,7 +91,7 @@ if(p<0){
 }
     }
 }
-*/
+
 //nhap vao 2 ky tu so sanhs 2 so va in ra ket qua man hinh
 public class Program
 {
@@ -116,3 +116,26 @@ n = Convert.ToInt32(Console.ReadLine());
             }
     }
 }
+*/
+public class Program
+{
+ private static void Main(string[] args)
+    {
+        int day =5;
+switch(day)
+{
+    case 1: System.Console.WriteLine("Chu nhat"); break;
+    case 2: System.Console.WriteLine("thu 2"); break;
+    case 3: System.Console.WriteLine("thu 3"); break;
+    case 4: System.Console.WriteLine("thu 4"); break;
+    case 5: System.Console.WriteLine("thu 5"); break;
+    case 6: System.Console.WriteLine("thu 6"); break;
+    case 7: System.Console.WriteLine("thu 7"); break;
+    default :
+    System.Console.WriteLine("Khong phai ngay trong tuan");
+    break;
+
+}
+    }
+}
+
