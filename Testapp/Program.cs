b4e09a8641efc815c2d48c9c0a21f7a3d533fd46
ngoan18 <Buikimngoan240@gmail.com>
2023-08-23 12:29:29 +0700

    taodoituong

---
+++ Testapp/Program.cs
@@ -0,0 +1,13 @@
+﻿using Testapp.Models;
+public class Program
+{
+    public static void Main(string [] args)
+    {
+        Nhansu ns =new Nhansu();
+        ns.Tenns = "Ha Han";
+        ns.Chucvu = "Truong phong";
+        ns.Phongban = "Ke toan";
+        ns. Luong = "320";
+        ns.Display();
+    }
+}
