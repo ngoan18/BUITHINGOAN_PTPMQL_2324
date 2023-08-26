96f11ff3ec1e4888a99e794387152f58d9da0423
ngoan18 <Buikimngoan240@gmail.com>
2023-08-26 09:41:32 +0700

    hang so

--- Testapp/Program.cs
+++ Testapp/Program.cs
@@ -3,12 +3,12 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        int Tuoi = 5;
-        string hoten = "Tran van tam";
-        float luong = 230;
+        const int kinhnghiem = 5;
+        const string hoten = "Nguyen van hop";
+       const float luong = 2500000;
 
 
-       Console.WriteLine("Tuoi:{0} - {1} - {2} vnd", Tuoi, hoten, luong);
+       Console.WriteLine("{0} nam - {1} - {2} vnd", kinhnghiem, hoten, luong);
 
     }
 }
