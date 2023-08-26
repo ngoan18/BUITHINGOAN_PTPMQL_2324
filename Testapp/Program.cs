d2c81961cf77acd16bb39f9006160ebc9965eae4
ngoan18 <Buikimngoan240@gmail.com>
2023-08-26 11:45:51 +0700

    lenh continue

--- Testapp/Program.cs
+++ Testapp/Program.cs
@@ -2,11 +2,10 @@
 {
     private static void Main(string[] args)
     {
-   
         for (int a=0; a<10; a++)
     {
-        if(a==5) break;
-        System.Console.WriteLine("vong lap thu {0}",a);
+        if(a==5) continue;
+        System.Console.WriteLine("vong lap thu {0}",j);
     }
     }
 }
