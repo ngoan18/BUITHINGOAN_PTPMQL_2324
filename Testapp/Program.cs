3f1562fc96aa38ba5ee9301f1d5ba0b455055aca
ngoan18 <Buikimngoan240@gmail.com>
2023-08-26 10:30:18 +0700

    chuyen doi kieu duw lieu

--- Testapp/Program.cs
+++ Testapp/Program.cs
@@ -1,14 +1,35 @@
-﻿
-public class Program 
+﻿/*
+public class Program
 {
-    public static void Main(string[] args)
+private static void Main(string[] args)
+
     {
-        const int kinhnghiem = 5;
-        const string hoten = "Nguyen van hop";
-       const float luong = 2500000;
+        string STR ="1123";
+        int a = int.Parse(STR);
+        Console.WriteLine("a =" +a);
+    }
+}
 
 
-       Console.WriteLine("{0} nam - {1} - {2} vnd", kinhnghiem, hoten, luong);
+public class Program
+{
+private static void Main(string[] args)
 
+    {
+        string STR ="123";  // gia tri vao chuyen doi
+        int ketqua;          // bien luu ket qua
+        bool kiemtra =false; // bien kiem tra
+        kiemtra = int.TryParse(STR,out(ketqua));  // chuyen doi du lieu
+        Console.WriteLine("Ket qua =" +ketqua);
+    }
+}
+*/
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        string str = "456";
+        int a = Convert.ToInt32(str);
+        Console.WriteLine("a = " + a);
     }
 }
