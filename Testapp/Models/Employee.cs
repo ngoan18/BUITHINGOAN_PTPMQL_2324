fba86c4611042b253bfb3ab12e50f498271e4ec4
ngoan18 <Buikimngoan240@gmail.com>
2023-08-26 15:11:03 +0700

    class Employee

---
+++ Testapp/Models/Employee.cs
@@ -0,0 +1,29 @@
+namespace Testapp.Models
+{
+    public class Employee
+    {
+        public string MaNV { get; set;}
+        public string TenNV { get; set;}
+        public int Tuoi { get; set;}
+        public string Luong { get; set;}
+        public void EnterData()
+        {
+            System.Console.WriteLine("MaNV");
+            MaNV = Console.ReadLine();
+            System.Console.WriteLine("TenNV");
+            TenNV = Console.ReadLine();
+            System.Console.WriteLine("Tuoi");
+            Tuoi = Convert.ToInt32(Console.ReadLine());
+            System.Console.WriteLine("Luong");
+            Luong = Console.ReadLine();
+        }
+        public void Display()
+        {
+            System.Console.WriteLine("MSV:{0}-{1}-{2} tuoi-{3} vnd",MaNV,TenNV, Tuoi, Luong);
+        }
+
+
+    }
+
+
+}
--- Testapp/Program.cs
+++ Testapp/Program.cs
@@ -3,10 +3,11 @@ public class Program
 {
     private static void Main(string[] args)
     {
-     Person pr = new Person();   
-     pr.Name = "Nguyen hong a";
-     pr.Address = "Ha Noi";
-     pr.Age = 22;
-     pr.Display();
+     Employee emp = new Employee();  
+     emp.MaNV =" 1921050439";
+     emp.TenNV =" Nguyen van B";
+     emp.Tuoi = 25;
+     emp. Luong ="250000000";
+     emp.Display();
     }
 }
