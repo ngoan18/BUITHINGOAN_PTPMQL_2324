db9810ba594cf13e3523b44aae1ffb7475ccad05
ngoan18 <Buikimngoan240@gmail.com>
2023-08-26 11:18:01 +0700

    cau lenh swith case

--- Testapp/Program.cs
+++ Testapp/Program.cs
@@ -1,28 +1,21 @@
 ﻿public class Program
 {
-    private static void Main(string[] args)
+    public static void Main(string[] args)
     {
-        float a = 3;
-        if(a>=8)
-        {
-            System.Console.WriteLine("Hoc sinh gioi");
-        }
-        else if(a>= 6.5)
-        {
-            System.Console.WriteLine("Hoc sinh kha");
-        }
-        else if(a>=5)
-        {
-            System.Console.WriteLine("Hoc sinh trung binh");
-        }
-        else if(a>=3.5)
-        {
-            System.Console.WriteLine("Hoc sinh yeu");
-        }
-        else
-        {
-            System.Console.WriteLine("Hoc sinh kem");
-        }
+    int day =2;
+    switch(day)
+    {
+        case 1: System.Console.WriteLine("Chu nhat"); break;
+        case 2: System.Console.WriteLine("thu 2"); break;
+        case 3: System.Console.WriteLine("thu 3"); break;
+        case 4: System.Console.WriteLine("thu 4"); break;
+        case 5: System.Console.WriteLine("thu 5"); break;
+        case 6: System.Console.WriteLine("thu 6"); break;
+        case 7: System.Console.WriteLine("thu 7"); break;
+        default :
+        System.Console.WriteLine("Khong phai ngay trong tuan");
+        break;
 
+    }
     }
 }
