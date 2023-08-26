f9ebcc2517645815f49b3e0b4b9e5fab35224f7d
ngoan18 <Buikimngoan240@gmail.com>
2023-08-26 11:27:07 +0700

    vong lap for

--- Testapp/Program.cs
+++ Testapp/Program.cs
@@ -1,21 +1,10 @@
 ﻿public class Program
 {
-    public static void Main(string[] args)
+    private static void Main(string[] args)
     {
-    int day =2;
-    switch(day)
+        for (int i=0; i< 10; i++)
     {
-        case 1: System.Console.WriteLine("Chu nhat"); break;
-        case 2: System.Console.WriteLine("thu 2"); break;
-        case 3: System.Console.WriteLine("thu 3"); break;
-        case 4: System.Console.WriteLine("thu 4"); break;
-        case 5: System.Console.WriteLine("thu 5"); break;
-        case 6: System.Console.WriteLine("thu 6"); break;
-        case 7: System.Console.WriteLine("thu 7"); break;
-        default :
-        System.Console.WriteLine("Khong phai ngay trong tuan");
-        break;
-
+        System.Console.WriteLine("vong lap thu {0}", i);
     }
     }
 }
