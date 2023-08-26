474ae3f5a59e504cb36be78b47dc8657087ea71b
ngoan18 <Buikimngoan240@gmail.com>
2023-08-26 09:32:42 +0700

    bien so

--- Testapp/Program.cs
+++ Testapp/Program.cs
@@ -1,13 +1,14 @@
-﻿using Testapp.Models;
-public class Program
+﻿
+public class Program 
 {
-    public static void Main(string [] args)
+    public static void Main(string[] args)
     {
-        Nhansu ns =new Nhansu();
-        ns.Tenns = "Ha Han";
-        ns.Chucvu = "Truong phong";
-        ns.Phongban = "Ke toan";
-        ns. Luong = "320";
-        ns.Display();
+        int Tuoi = 5;
+        string hoten = "Tran van tam";
+        float luong = 230;
+
+
+       Console.WriteLine("Tuoi:{0} - {1} - {2} vnd", Tuoi, hoten, luong);
+
     }
 }
