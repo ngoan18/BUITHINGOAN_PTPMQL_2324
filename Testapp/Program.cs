0c5d6334d7e11f0e017aa64a2ef36a253c20192e
ngoan18 <Buikimngoan240@gmail.com>
2023-08-26 10:48:27 +0700

    tinh tong hieu tich thuong

--- Testapp/Program.cs
+++ Testapp/Program.cs
@@ -1,37 +1,16 @@
-﻿
-public class Program
-{
-private static void Main(string[] args)
-
-    {
-        string STR ="1123";
-        int a = int.Parse(STR);
-        Console.WriteLine("a =" +a);
-    }
-}
-
-************
-public class Program
-{
-private static void Main(string[] args)
-
-    {
-        string STR ="123";  // gia tri vao chuyen doi
-        int ketqua;          // bien luu ket qua
-        bool kiemtra =false; // bien kiem tra
-        kiemtra = int.TryParse(STR,out(ketqua));  // chuyen doi du lieu
-        Console.WriteLine("Ket qua =" +ketqua);
-    }
-}
-
-*************
-
-public class Program
+﻿public class Program
 {
     private static void Main(string[] args)
     {
-        string str = "456";
-        int a = Convert.ToInt32(str);
-        Console.WriteLine("a = " + a);
+        int a, b;
+        System.Console.Write("a=");
+        a = Convert.ToInt32(Console.ReadLine());
+        System.Console.Write("b=");
+        b = Convert.ToInt32(Console.ReadLine());
+        System.Console.WriteLine("{0} + {1} ={2}", a,b, a+b);
+        System.Console.WriteLine("{0} - {1} ={2}", a,b, a-b);
+        System.Console.WriteLine("{0} * {1} ={2}", a,b, a*b);
+        System.Console.WriteLine("{1} / {0} ={2}", a,b, a/b);
+        System.Console.WriteLine("{0} % {1} ={2}", a,b, a%b);
     }
 }
