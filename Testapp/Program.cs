25b73570275ff48f33b31775a005e0c677ecf93a
ngoan18 <Buikimngoan240@gmail.com>
2023-08-27 17:47:02 +0700

    pt

--- Testapp/Program.cs
+++ Testapp/Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿using Testapp.Models;
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        Giaipt ptb1 = new Giaipt();
+        ptb1.a=2;
+        ptb1.b=5;
+        ptb1.Giaiptb1();
+        
+    }
+}
