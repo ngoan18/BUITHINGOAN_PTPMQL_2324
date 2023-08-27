186016386970fda6ff3038ee18f087f790fb42f0
ngoan18 <Buikimngoan240@gmail.com>
2023-08-27 16:21:01 +0700

    gpt

--- Testapp/Program.cs
+++ Testapp/Program.cs
@@ -1,14 +1,1 @@
-﻿using Console.Models;
-using Testapp.Models;
-public class Program
-{
-    private static void Main(string[] args)
-    {
-        Giaipt ptb2 = new Giaipt();
-        ptb2.a=2;
-        ptb2.b=5;
-        ptb2.c=3;
-        ptb2.Giaiptb2();
-        
-    }
-}
+﻿
