88fee1842caa21da27434c40aa51580e7df4f598
ngoan18 <Buikimngoan240@gmail.com>
2023-08-27 22:57:23 +0700

    demo

---
+++ Testapp/Models/Fruit.cs
@@ -0,0 +1,39 @@
+namespace Testapp.Models;
+
+public class Fruit
+
+{
+
+    public string FullName { get; set; }
+    public string Mau { get; set; }
+    public string Soluong { get; set; }
+
+
+   public void EnterData()
+
+    {
+
+        System.Console.Write("FullName =");
+        FullName = System.Console.ReadLine();
+        System.Console.Write("Mau");
+        Mau= System.Console.ReadLine();
+        System.Console.Write("Soluong =");
+        Soluong = System.Console.ReadLine();
+
+    }
+
+    public void Display()
+
+    {
+
+        System.Console.Write("{0}-mau:{1}- so luong:{2}", FullName,Mau, Soluong);
+
+    }
+
+
+
+
+
+
+
+}
---
+++ Testapp/Program.cs
@@ -0,0 +1,12 @@
+﻿using Testapp.Models;
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        Giaipt ptb2 = new Giaipt();
+        ptb2.a=2;
+        ptb2.b=5;
+        ptb2.Giaiptb2();
+        
+    }
+}
