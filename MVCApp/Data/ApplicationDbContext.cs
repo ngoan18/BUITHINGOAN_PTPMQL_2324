f37d83381587679ce0dd01ffb1d1c57584564eb0
ngoan18 <Buikimngoan240@gmail.com>
2023-09-24 12:30:03 +0700

    sinh code tu dong class Daily

--- MVCApp/Data/ApplicationDbContext.cs
+++ MVCApp/Data/ApplicationDbContext.cs
@@ -15,5 +15,7 @@ namespace MVCApp.Data
         }
 
         public DbSet<MVCApp.Models.Employee> Employee { get; set; } = default!;
+
+        public DbSet<MVCApp.Models.Daily> Daily { get; set; } = default!;
     }
 }
--- MVCApp/Migrations/20230924052705_Table_Daily.Designer.cs
+++ MVCApp/Migrations/20230924052705_Table_Daily.Designer.cs
@@ -2,6 +2,7 @@
 using MVCApp.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -9,9 +10,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace MVCApp.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230924052705_Table_Daily")]
+    partial class Table_Daily
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "7.0.11");
